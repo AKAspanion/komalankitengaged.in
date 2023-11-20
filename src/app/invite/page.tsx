4d64c06c0beaf85f3e62effb39d45f4607fe27ad
Ankit Pandit(Consultant) <Ankit.Pandit@ril.com>
2023-11-21 04:44:28 +0530

    minor style tweaks

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -5,8 +5,9 @@ import classNames from "classnames";
 
 import Image from "next/image";
 
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import "./landing.css";
-import { useState } from "react";
 
 const marley = localFont({
   src: "../../../public/fonts/marley/ttf/marley-marley-regular-lovely-script-400.ttf",
@@ -19,10 +20,24 @@ const helostar = localFont({
 
 function Invite() {
   const [loaded, setLoaded] = useState(false);
+  const [showRSVP, setShowRSVP] = useState(false);
 
   const handleOnLoad = () => {
     setLoaded(true);
   };
+
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (searchParams) {
+      const rsvp = searchParams.get("rsvp");
+
+      if (rsvp) {
+        setShowRSVP(true);
+      }
+    }
+  }, [searchParams]);
+
   return (
     <div className={classNames("relative bg-slate-50 text-sm text-black")}>
       <div className="absolute bg-slate-50 top-0 w-screen h-screen">
@@ -39,21 +54,9 @@ function Invite() {
       </div>
 
       <div className="z-10 w-screen absolute text-center h-screen flex flex-col items-center justify-between gap-16 p-8">
-        <div className="">
-          {/* <div>JOIN US FOR</div>
-          <div>OUR ENGAGEMENT CELEBRARTION</div> */}
-        </div>
+        <div></div>
         <div className="text-center flex flex-col items-center justify-center">
-          <p
-            className={classNames(
-              "text-2xl p-5 rounded w-fit",
-              marley.className
-            )}
-          >
-            Celebrating the Enaggement of
-          </p>
           <div
-            // style={{ textShadow: "1px 1px 4px black" }}
             style={{ textShadow: "0.5px 0.5px 2px black" }}
             className={classNames(
               "text-5xl sm:text-6xl md:text-7xl lg:text-8xl drop-shadow-xl",
@@ -62,11 +65,33 @@ function Invite() {
           >
             <div>Komal & Ankit</div>
           </div>
+          <p
+            className={classNames(
+              "text-2xl md:text-3xl rounded w-fit",
+              marley.className
+            )}
+          >
+            are getting engaged!
+          </p>
         </div>
         <div className="text-center flex flex-col items-center justify-center">
-          {/* <div className={classNames("p-4 text-2xl", mantulPro.className)}>
-            We are getting engaged!
-          </div> */}
+          {showRSVP ? (
+            <div className="m-6 tracking-wide font-medium rounded bg-opacity-20 bg-gray-900 p-4">
+              <div>Will you be attending?</div>
+              <div className="pt-2 flex justify-between">
+                <a href={`/invite/rsvp?id=${searchParams?.get("rsvp") || ""}`}>
+                  <button className="border-2 border-black px-2 py-1">
+                    <div>Yes</div>
+                  </button>
+                </a>
+                <a href={`/invite/rsvp?id=${searchParams?.get("rsvp") || ""}`}>
+                  <button className="border-2 border-black px-2 py-1">
+                    <div>No</div>
+                  </button>
+                </a>
+              </div>
+            </div>
+          ) : null}
           <div
             className={classNames(
               "leading-6 tracking-widest px-4 py-1 rounded w-fit"
@@ -77,8 +102,6 @@ function Invite() {
                 1.10.2023
               </div>
             </a>
-            {/* <div>December 1st, 2023</div> */}
-            {/* <div className="divider" /> */}
           </div>
         </div>
       </div>
