9a783df49c5fc945dcf9d7b3e988203d74e40050
Ankit Kumar Pandit <panditankit1995@gmail.com>
2024-02-09 21:26:37 +0530

    MInor fix in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,8 @@ export function middleware(request: NextRequest) {
   const targetDate = new Date("12.02.2023 19:00").getTime();
   const currentDate = new Date().getTime();
 
+  // return NextResponse.next();
   if (targetDate > currentDate) {
-    return NextResponse.next();
   } else {
     return NextResponse.redirect(new URL("/api/auth/over", request.url));
   }
@@ -14,8 +14,8 @@ export function middleware(request: NextRequest) {
 // Supports both a single string value or an array of matchers
 export const config = {
   matcher: [
-    // "/api/guest/:path*",
-    // "/api/guest/[id]/rsvp/:path*",
-    "/((?!api|static|.*\\..*|_next).*)",
+    "/api/guest/:path*",
+    "/api/guest/[id]/rsvp/:path*",
+    // "/((?!api|static|.*\\..*|_next).*)",
   ],
 };
